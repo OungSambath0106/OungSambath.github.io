99b12be5873952ce11a8f0753a125d6d32aa987e
OungSambath0106 <132065974+OungSambath0106@users.noreply.github.com>
2023-04-28 18:44:01 +0700

    Add files via upload

---
+++ Js/Login.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector('.wrapper');
+const loginlink = document.querySelector('.login-link');
+const registerlink = document.querySelector('.register-link');
+const btnLogin = document.querySelector('.btnLogin');
+const iconClose = document.querySelector('.icon-close');
+
+registerlink.addEventListener('click', ()=>
+{
+    wrapper.classList.add('active')
+});
+
+loginlink.addEventListener('click', ()=>
+{
+    wrapper.classList.remove('active')
+});
+
+btnLogin.addEventListener('click', ()=>
+{
+    wrapper.classList.add('active-popup')
+});
+
+iconClose.addEventListener('click', ()=>
+{
+    wrapper.classList.remove('active-popup')
+});
